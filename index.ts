fbea2b883d68f23ca5a8f62d381928bb1e0f8292
DeMarc Johnson <demarcjohnson@gmail.com>
2026-02-10 17:27:18 -0500

    Added a has_layers for general layers being in the active comp

--- index.ts
+++ index.ts
@@ -4,8 +4,4 @@ import "./library/array";
 import "./library/json";
 import * as lib from "./library/library";
 
-// var sayHello = () => alert("Hello from ExtendScript");
-var sayHello = () => {
-  lib.print(`hey`);
-  lib.print(lib.all_layers());
-};
+var sayHello = () => alert("Hello from ExtendScript");
